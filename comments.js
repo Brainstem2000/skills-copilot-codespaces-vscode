6a96fd0cbee791d0b9498d1dbf24690c013608bf
Brainstem2000 <Brainstem2000@users.noreply.github.com>
2024-02-08 21:22:37 +0000

    Copilot corrected third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,12 @@
 // Create web server
-// 1. Create a web server
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const querystring = require('querystring');
+const comments = require('./comments');
+
+const server = http.createServer((req, res) => {
+  const { pathname, query } = url.parse(req.url, true);
+  const method = req.method;
+  
