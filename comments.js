a40b02485503926b28138c6857e609d81f59c2e6
Brainstem2000 <Brainstem2000@users.noreply.github.com>
2024-02-08 21:25:50 +0000

    Updated again - fixed error

--- comments.js
+++ comments.js
@@ -7,6 +7,7 @@ const querystring = require('querystring');
 const comments = require('./comments');
 
 const server = http.createServer((req, res) => {
-  const { pathname, query } = url.parse(req.url, true);
-  const method = req.method;
+    const { pathname, query } = url.parse(req.url, true);
+    const method = req.method;
+}); // Add closing curly brace here
   
